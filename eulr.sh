c66265481368dfc6df49265fafc1b24932546632
Said Pelaez <diego.210@hotmail.es>
2016-11-14 10:57:29 -0600

    Updating

--- eulr.sh
+++ eulr.sh
@@ -23,4 +23,31 @@ case "$1" in
         git commit -m $2
         git push origin master
     ;;
+
+    g )
+        echo "<?php
+    require_once 'oom/OOM.php';
+
+    class $2 extends OOM{
+        function __construct(){
+            $this->model_name = $2;
+            $this->private = [];
+        }
+    }
+    $2 = new $2(); // Optional, but cool.
+    ?>" > php/model/$2.php
+
+        echo "<?php
+    require_once('php/model/$2.php');
+    require_once('helpers.php');
+    require_once('errors.php');
+    session_start();
+    /**
+    * $2Controller class
+    */
+    class $2Controller{}?>" > php/controller/$2.php
+
+        mkdir views/$2
+
+    ;;
 esac
